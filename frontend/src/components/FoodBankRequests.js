18d400c4563d8b27aa1210c1c134d9cd6a3a309a
ameen <ameen.izhac@gmail.com>
2022-07-02 23:24:25 +0100

    updated FoodBankRequestsPage

--- frontend/src/components/FoodBankRequests.js
+++ frontend/src/components/FoodBankRequests.js
@@ -16,28 +16,33 @@ function FoodBankRequests() {
 
     return(
         <div>
-            <h2>Your requests</h2>
-            <p>when you receive requests for help, they will appear here</p>
+            <div className="pageContent">
+                <h2 className="yourRequests">Your requests</h2>
+                <p className="yourRequestsSubText">when you receive requests for help, they will appear here</p>
+            </div>
             <div>
-            {items.map(item => (
-                <div className='card' key='item.email'>
-
-
+                {items.map(item => (
+                    <div className='card' key={item.email}>
                         <div className='purpleLine'></div>
-
-
-                    <div className="textDiv">
-                        <p>{item.requestedFoodBank}</p>
-                        <p>{item.requestingOrganization}</p>
-                        <p>{item.email}</p>
-                        <p>{item.needQuantity}</p>
-                        <p>{item.helpType}</p>
-                        <p>{item.requestingOrganizationType}</p>
-                        <p>{item.label}</p>
+                        <div className="textDiv">
+                        <div>
+                            <span className='cardTitle'>{item.requestedFoodBank}</span> 
+                            <span className="cardTitleSubText">has sent a request for help</span>
+                        </div>
+                        <div className="quantityContainer"> 
+                            <div className='quantityLabel'>Number of people in need:</div> 
+                            <div className="quantityBox">
+                                <p className="quantityText">{item.needQuantity}</p>
+                            </div> 
+                        </div>
+                            {/* <p>{item.requestingOrganization}</p>
+                            <p>{item.email}</p>
+                            <p>{item.helpType}</p>
+                            <p>{item.requestingOrganizationType}</p>
+                            <p>{item.label}</p> */}
+                        </div>
                     </div>
-                </div>
-            ))
-            }
+                ))}
             </div>
         </div>
     );
