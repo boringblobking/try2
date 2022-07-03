18890d0c3f263bf45e963633bdb7279f65cbbab2
ameen <ameen.izhac@gmail.com>
2022-07-03 15:30:45 +0100

    continued with FoodBankRequests page

--- frontend/src/components/FoodBankRequests.js
+++ frontend/src/components/FoodBankRequests.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './FoodBankRequests.css'
+import smallLogo from '../images/logo.png'
 
 function FoodBankRequests() {
     useEffect( ()=> {
@@ -15,32 +16,52 @@ function FoodBankRequests() {
     };
 
     return(
-        <div>
-            <div className="pageContent">
+        <div className="pageContent">
+            <nav className='navbar'>
+                <div className='full-logo'>
+                    <img src={smallLogo} className='logo'/> 
+                    <h3 className='logo-txt'><span className='red'>Food</span><span className='purple'>&Flow</span></h3>
+                </div>
+
+                <ul className='nav-links'>
+                    <li><strong>Home</strong></li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <br className="topGap"></br>
+            <div className="welcomeBox">
+                <h2 className="welcomeMessage">Welcome, Liverpool Street Food Bank</h2>
+                <p className="welcomeSubText">Use this area to track and receive requests for help.</p>
+            </div>
+            <div>
                 <h2 className="yourRequests">Your requests</h2>
                 <p className="yourRequestsSubText">when you receive requests for help, they will appear here</p>
             </div>
             <div>
                 {items.map(item => (
-                    <div className='card' key={item.email}>
-                        <div className='purpleLine'></div>
-                        <div className="textDiv">
-                        <div>
-                            <span className='cardTitle'>{item.requestedFoodBank}</span> 
-                            <span className="cardTitleSubText">has sent a request for help</span>
+                    <div>
+                        <div className='card' key={item.email}>
+                            <div className='purpleLine'></div>
+                            <div className="textDiv">
+                            <div>
+                                <span className='cardTitle'>{item.requestedFoodBank}</span> 
+                                <span className="cardTitleSubText">has sent a request for help</span>
+                            </div>
+                            <div className="quantityContainer"> 
+                                <div className='quantityLabel'>Number of people in need:</div> 
+                                <div className="quantityBox">
+                                    <p className="quantityText">{item.needQuantity}</p>
+                                </div> 
+                            </div>
+                                {/* <p>{item.requestingOrganization}</p>
+                                <p>{item.email}</p>
+                                <p>{item.helpType}</p>
+                                <p>{item.requestingOrganizationType}</p>
+                                <p>{item.label}</p> */}
+                            </div>
                         </div>
-                        <div className="quantityContainer"> 
-                            <div className='quantityLabel'>Number of people in need:</div> 
-                            <div className="quantityBox">
-                                <p className="quantityText">{item.needQuantity}</p>
-                            </div> 
-                        </div>
-                            {/* <p>{item.requestingOrganization}</p>
-                            <p>{item.email}</p>
-                            <p>{item.helpType}</p>
-                            <p>{item.requestingOrganizationType}</p>
-                            <p>{item.label}</p> */}
-                        </div>
+                        <br></br>
                     </div>
                 ))}
             </div>
