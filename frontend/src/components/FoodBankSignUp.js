460be05093524bad5c177165510e147e974e353c
ameen <ameen.izhac@gmail.com>
2022-07-18 00:17:41 +0100

    password match checking now works for food-bank-sign-up

--- frontend/src/components/FoodBankSignUp.js
+++ frontend/src/components/FoodBankSignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './FoodBankSignUp.css'
 import './LandingPage.css'
 import smallLogo from '../images/logo.png'
@@ -6,6 +6,9 @@ import bigLogo from '../images/big-logo.png'
 import HeroShot from '../images/HeroShot.png'
 
 function BeneficiarySignUp() {
+    const [show, setShow] = useState(false)
+    const [pw1, setPw1] = useState('')
+    const [pw2, setPw2] = useState('')
     return (
         <div className='landing-bdy'>
             <nav className='navbar'>
@@ -30,13 +33,17 @@ function BeneficiarySignUp() {
 
                     <form method="POST" action="/new-food-bank-account">
                         <div class="TextBox">
+                            {
+                                show ? <p className="ripPasswords">bro, the passwords don't even match mate</p> : null
+                            }
+                            <br />
                             <div><input name="name" type="text" placeholder="Name of centre or organisation" required /></div>
                             <br />
                             <div><input name="email" type="email" placeholder="Email" required /></div>
                             <br />
-                            <div><input name="password1" type="password" placeholder="Password" required /></div>
+                            <div><input value={pw1} onChange={e => {setPw1(e.target.value); setShow(false)}} name="password1" type="password" placeholder="Password" required /></div>
                             <br />
-                            <div><input name="password2" type="password" placeholder="Retype Password" required /></div>
+                            <div><input value={pw2} onChange={e => {setPw2(e.target.value); setShow(false)}} name="password2" type="password" placeholder="Retype Password" required /></div>
                             <br />
                             <div><input name="address" type="text" placeholder="Full address" required /></div>
                             <br />
@@ -44,7 +51,7 @@ function BeneficiarySignUp() {
                             <br />
                             <div><input name="helpType" type="text" placeholder="Type of help" required /></div>
                             <br />
-                            <button type="submit" value="Create Account">submit</button>
+                            <button onClick={()=>{if(pw1!=pw2) {setShow(true)}}} type="submit" value="Create Account">submit</button>
                             </div>
                     </form>
                 </div>
